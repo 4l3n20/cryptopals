9bf1add9a826cf03f086703410f59d8dd94d09bd
al_enzo <a.enzevaee@gmail.com>
2019-10-13 19:11:00 +1100

    string to base64 function is fixed base64 to sting function is fixed
    Signed-off-by: al_enzo <a.enzevaee@gmail.com>

--- hex2base64_vs.py
+++ hex2base64_vs.py
@@ -12,9 +12,11 @@
 
 from codecs import encode, decode
 import base64
+import binascii
 
 Message_hex = "Enter a Hex Value/String: "
 Message_bs64 = "Enter a base64 data string to convert to hex: "
+Message_bs64_2str = "Enter a base64 data string to convert to string: "
 Message_str = "Enter a String: "
 Err = "entered value must only contain Hex e.g.(0..9 - A..F)"
 
@@ -30,6 +32,9 @@ def read(i):                                                         # Read From
     if i == '3':
         strg = input(Message_str)
         return strg
+    if i == '4':
+        b642str = input(Message_bs64_2str)
+        return b642str
 
 
 def is_hexa(inp):                                                    # Check if input is an actual HEX value.
@@ -54,23 +59,31 @@ def hex2b64(i):                                                      # make sure
 def bas62hex(i):                                                     # Convert base64 to hex
     i = i
     bs = read(i)
-
     h3x = base64.b64decode(bs).hex()
     return h3x
 
 
 def str2bs64(i):
     i = i
-    str = read(i)
-    bas64 = encode(str, 'base64').decode()
+    str = read(i).encode('utf-8')
+    bas64 = base64.b64encode(str).decode('utf-8')
     return bas64
+
+
+def bs642str(i):
+    i = i
+    bs64 = read(i).encode('UTF-8')
+    str = base64.b64decode(bs64).decode('utf-8')
+
+    return str
 
 
 def menu():
     print("[1] Hex2base64")
     print("[2] base64-2-hex")
     print("[3] String2base64")
-    print("[q] Exit.  *Acceptable options (3, e, E, exit, Exit)")
+    print("[4] base64-2-String")
+    print("[q] Exit.  *Acceptable options (q, e, E, quit, exit, Exit)")
     print("Select from menu: ")
 
 
@@ -85,8 +98,10 @@ def main():
             print("OUTPUT: " + bas62hex(opt) + '\n')
             main()
         elif opt == '3':
-            print("OUTPUT: " + bas62hex(opt))
+            print("OUTPUT: " + str2bs64(opt) + '\n')
             main()
+        elif opt == '4':
+            print("OUTPUT: " + bs642str(opt) + '\n')
         print("Bye!")
         exit()
 
